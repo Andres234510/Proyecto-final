295ca080f16a65b525d49164753fdb622703c8e1
andres <andres2345v@gmail.com>
2025-05-08 19:15:14 -0500

    Citas

--- src/main/java/co/edu/uniquindio/javafxtestnoche/proyectofinal/model/personas/Administrador.java
+++ src/main/java/co/edu/uniquindio/javafxtestnoche/proyectofinal/model/personas/Administrador.java
@@ -1,4 +1,44 @@
 package co.edu.uniquindio.javafxtestnoche.proyectofinal.model.personas;
 
-public class Administrador {
+import co.edu.uniquindio.javafxtestnoche.proyectofinal.model.citas.Cita;
+
+import java.util.List;
+
+
+public class Administrador extends Persona {
+    // Constructor
+    public Administrador(String id, String nombre, String correo, String telefono) {
+        super(id, nombre, correo, telefono);
+    }
+
+    // Métodos de administración (los detalles se pueden ampliar en otras clases de servicio o controladores)
+
+    public void registrarMedico(List<Medico> lista, Medico nuevo) {
+        lista.add(nuevo);
+    }
+
+    public void eliminarMedico(List<Medico> lista, Medico medico) {
+        lista.remove(medico);
+    }
+
+    public void registrarPaciente(List<Paciente> lista, Paciente nuevo) {
+        lista.add(nuevo);
+    }
+
+    public void eliminarPaciente(List<Paciente> lista, Paciente paciente) {
+        lista.remove(paciente);
+    }
+
+    public void generarReporteCitas(List<Cita> citas) {
+        System.out.println("=== Reporte de Citas ===");
+        for (Cita cita : citas) {
+            System.out.println(cita);
+        }
+    }
+
+    // Implementación del método abstracto
+    @Override
+    public String getTipo() {
+        return "Administrador";
+    }
 }
